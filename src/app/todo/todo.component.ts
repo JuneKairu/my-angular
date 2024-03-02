7f99d2369bceb01bdafde2c976c8f14347b5a60e
junekairu <micadario0926@gmail.com>
2024-03-02 15:02:01 +0800

    update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Todo } from '../todo.model';
 
 @Component({
   selector: 'app-todo',
-  standalone: true,
-  imports: [],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
+  todos: Todo[] = [];
+  newTodo: string = '';
 
+  addTodo() {
+    if (this.newTodo.trim() === '') return;
+
+    const todo: Todo = {
+      id: this.todos.length + 1,
+      task: this.newTodo,
+      completed: false,
+    };
+
+    this.todos.push(todo);
+    this.newTodo = '';
+  }
 }
