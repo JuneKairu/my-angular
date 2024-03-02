8287bef01a948f6da94fb274b6ab7a1d7b48c601
junekairu <micadario0926@gmail.com>
2024-03-02 14:42:11 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsComponent } from './forms/forms.component';
+import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
-  { path: 'forms', component: FormsComponent }
+  { path: 'forms', component: TodoComponent }
 ];
 
 @NgModule({
--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormsComponent } from './forms.component';
+import { TodoComponent } from './todo.component';
 
-describe('FormsComponent', () => {
-  let component: FormsComponent;
-  let fixture: ComponentFixture<FormsComponent>;
+describe('TodoComponent', () => {
+  let component: TodoComponent;
+  let fixture: ComponentFixture<TodoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormsComponent]
+      imports: [TodoComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(FormsComponent);
+    fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
