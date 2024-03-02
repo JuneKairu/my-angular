11f853792d8094cd9fcb9a62af0ef6272c135e62
junekairu <micadario0926@gmail.com>
2024-03-02 13:50:26 +0800

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { SkillsComponent } from './skills/skills.component';
-import { WorksComponent } from './works/works.component';
+import { Forms } from './forms/forms.component';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'works', component: WorksComponent },
+  { path: 'forms', component: Forms }
 ];
 
 @NgModule({
